64cb5b7216e11572b20f3432fc93e1fdebe95018
jchtd <jchtd@users.noreply.github.com>
2020-09-10 20:14:35 +0800

    Update Java and Kotlin files

--- hello-world/src/main/kotlin/com/oracle/netsuite/jeck/helloWorld.kt
+++ hello-world/src/main/kotlin/com/oracle/netsuite/jeck/helloWorld.kt
@@ -1,34 +1,60 @@
 package com.oracle.netsuite.jeck
 
+import java.math.BigDecimal
 import java.time.LocalDate
 
 fun main(){
-    // int a = 4
-    val a: Int = 4
-    val b = 5 // type inference
-    val c = "A"
-//    a = 7 // vals are immutable and cannot be changed
+    val basicInt: Int = 4
+    val basicIntNoTypeDeclaration = 5 // type inference
+    val basicString = "A"
+//    basicInt = 7 // vals are immutable and cannot be changed
 
-    var d = true // var are mutable and can be changed anytime
-//    d = "a" // mismatch due to inference
-    d = false
+    var basicBoolean = true // var are mutable and can be changed anytime
+//    basicBoolean = "a" // mismatch due to inference (basicBoolean is of boolean type)
+    basicBoolean = false
 
-    var e = "hello world"
-    e = e.toUpperCase()
-    e = e.toLowerCase()
+    var helloWorldString = "hello world"
+    println(helloWorldString)
+    helloWorldString = helloWorldString.toUpperCase()
+    helloWorldString = helloWorldString.toLowerCase()
 
-    val f: LocalDate = LocalDate.now()
-    val h: Boolean // this is better if you're declaring the variable first but not assigning it a value
-    var i: String  // i.e. to be assigned later, so developer does not forget its intended data type
-    if(f.isLeapYear) {
-        h = true
-        i = "A"
+    val dateNow: LocalDate = LocalDate.now()
+    val typeInitializedBoolean: Boolean // this is better if you're declaring the variable first but not assigning it a value
+    var typeInitializedString: String  // i.e. to be assigned later, so developer does not forget its intended data type
+    if(dateNow.isLeapYear) {
+        typeInitializedBoolean = true
+        typeInitializedString = "A"
     } else {
-        h = false
-        i = "B"
+        typeInitializedBoolean = false
+        typeInitializedString = "B"
     }
 
-    e = "hello world $a"
+    helloWorldString = "$helloWorldString $basicInt."
 
-    println(e)
+    val underscoredNumberIsStillNumber = 4_000_000 // you can use underscores for numbers to make it more readable, but underlying value is the same as 4 million
+
+    helloWorldString = "$helloWorldString there are $underscoredNumberIsStillNumber reasons why things are like this"
+    println(helloWorldString)
+
+    // has other number types such as float, double and long (long int)
+    // can also use Java's BigDecimal for more precision
+    val bigDecimalTen = BigDecimal.TEN
+    val bigDecimalOfNumber = BigDecimal(88.554423)
+
+    // nulls - add ? to signify it's nullable
+    val nullableString: String?
+    // which means that all other variables declared so far were non-nullable.
+//    underscoredNumberIsStillNumber = null // non-null error
+
+    val listOfStrings: List<String> = listOf() // empty list, since this is val, it's gonna be an empty list
+    val listOfWhatevers: List<Any> = listOf() // Any is like an open data type, it can be anything. ---> THIS IS NOT A SUGGESTED PRACTICE.
+
+    // we want to have the ability to add values to it down the line. can we use var list? NO. It would be like replacing the list everytime.
+    // There is another way: mutableList
+    // There are two ways to define lists (and sets):
+    val mutableListOfStrings: MutableList<String> = mutableListOf()
+    val mutableListOfString2 = mutableListOf<String>()
+
+    mutableListOfStrings.add("A")
+    println("The first element is ${mutableListOfStrings[0]}") // if the variable is not one word (such as list[i]), enclose this in curly braces
 }
